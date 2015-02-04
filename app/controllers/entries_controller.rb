346c7e75a9bf06d8af4255939eb9855d1e4a8542
Marc Mathieu <goheat631@gmail.com>
2015-02-04 14:15:33 -0500

    added a create form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,4 +5,9 @@ class EntriesController < ApplicationController
 		# @entries = @project.monthly_work(current_month) 
 		@entries = @project.entries.where(date: @date.beginning_of_month..@date.end_of_month)
 	end
+
+	def new
+		@project = Project.find params[:project_id]
+		@entry = @project.entries.new
+	end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,11 +3,11 @@ class Entry < ActiveRecord::Base
 	belongs_to :project
 	validates :hours, {numericality: true}
 	validates :minutes, {numericality: true}
-	validates :date presence: true
-	validates :hours presence: true
-	validates :minutes presence: true
+	validates :date, presence: true
+	validates :hours, presence: true
+	validates :minutes, presence: true
+	validates_associated :project
 
-	validates :validates_associated :project
 
 
 end
