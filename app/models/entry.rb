5cfc27b8d061d921ac7b2ca821be0e4b55ba69f2
Marc Mathieu <goheat631@gmail.com>
2015-02-04 11:54:15 -0500

    added entry and project vaildations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,13 @@
 class Entry < ActiveRecord::Base
 
 	belongs_to :project
+	validates :hours, {numericality: true}
+	validates :minutes, {numericality: true}
+	validates :date presence: true
+	validates :hours presence: true
+	validates :minutes presence: true
+
+	validates :validates_associated :project
+
 
 end
